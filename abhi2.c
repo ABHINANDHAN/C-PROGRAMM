d02dc0d40e7f37bf895a986f1dd40b46c6c31ccb
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-02-02 16:20:34 +0530

    Create abhi2.c

---
+++ abhi2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void printHelloWorld();
+int main(){
+    printHelloWorld();
+    return 0;
+}
+/*
+ *  Function to print "Hello World"
+ */
+void printHelloWorld(){
+    printf("Hello World\n");
+}
