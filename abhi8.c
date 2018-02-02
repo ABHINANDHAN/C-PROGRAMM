3ec6de56ae35fa86d8d8ddf333aa03fee3c54b8a
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-02-02 16:35:01 +0530

    Create abhi8.c

---
+++ abhi8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+int n, num, rev = 0;
+printf("Enter any number to check palindrome ");
+scanf("%d", &n);
+num = n; 
+while(n != 0)
+{
+rev = (rev * 10) + (n % 10);
+ n  /= 10;
+}
+if(rev == num)
+{
+printf("%d is palindrome.", num);
+}
+else
+{
+printf("%d is not palindrome.", num);
+}
+}
