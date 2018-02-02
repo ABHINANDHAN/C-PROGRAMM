2529c1c66936ae1f3eb45b8702d70cb381d2876b
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-02-02 16:33:51 +0530

    Create abhi7.c

---
+++ abhi7.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<math.h>
+ 
+long power (inta, intb);
+ 
+int main()
+{
+    int power=4, num=2;
+    long result;
+ 
+    printf("%d", a);
+    scanf("%d", &b);
+    printf("%d", a);
+    scanf("%d", &a);
+    result = power(num, pow);
+    printf("%d^%d is %ld", b, a, result);
+    return 0;
+}
+ 
+long power (int a, int b)
+{
+    if (a)
+    {
+        return (b * a(b ,a- 1));
+    }
+    return 1;
+}
