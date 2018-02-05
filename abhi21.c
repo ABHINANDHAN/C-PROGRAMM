69cc8364717038c2b36619acbb6881d3b82eabcb
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-02-05 14:23:09 +0530

    Create abhi21.c

---
+++ abhi21.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int a[10],i,j,t,n;
+  
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+  
+	{
+		scanf("%d",&a[i]);
+		
+	}
+	for(i=0;i<n-1;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+			t=a[i];
+			a[i]=a[j];
+			a[j]=t;
+			}
+	}
+	}
+	printf("%d",a[0]);
+	return 0;
+}
