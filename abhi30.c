57681b3fcb330de659499a65d430d1c706de7895
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-02-07 14:18:02 +0530

    Create abhi30.c

---
+++ abhi30.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,j,k,l,m,n;
+printf("enter the first time:");
+scanf(%d%d,&i,&j0;
+printf("enter the second time:);
+scanf(%d%d,&k,&l);
+m=i-k;
+n=j-l;
+printf("%d%d",m,n);
+return();
+}
