16781f99c972540f118be9a0ca6ed61608c77012
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-02-07 14:39:10 +0530

    Create bibb.c

---
+++ bibb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+ char a[30];
+ int i, count=0;
+ printf("enter a paragraph");
+ scanf("%[^\n]s",&a);
+ for(i=0;a[i]!='\0';i++)
+ {
+ 
+ if(a[i]=='.')
+ 
+ {
+ count=count+1;
+ 
+ }
+ 
+ }
+ printf("number of lines=%d",count);
+
+}
