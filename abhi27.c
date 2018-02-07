33f7cd536041c77f5b520de285db7863fc539ffd
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-02-07 14:06:07 +0530

    Create abhi27.c

---
+++ abhi27.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+
+	char ch;
+  
+	scanf("%c",&ch);
+	if(ch=='0'||ch=='1'||ch=='2'||ch=='3'||ch=='4'||ch=='5'||ch=='6'||ch=='7'||ch=='8'||ch=='9')
+	{
+  
+		printf("yes");
+  }
+  
+else
+{
+	printf("no");
+}
+}
