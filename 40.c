bf55d34d49e1de584990106bdf665707e6fc7d34
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-02-19 13:23:41 +0530

    Create 40.c

---
+++ 40.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+int a=1,b=1,i,n,c;
+printf("\n enter the range to print the Fibonacci numbers:\n");
+scanf("%d",&n);
+printf("%d",a);
+printf("\t%d",b);
+for(i=1;i<=n-2;i++)
+{
+c=a+b;
+printf("\t%d",c);
+a=b;
+b=c;
+}
+return 0;
+}
