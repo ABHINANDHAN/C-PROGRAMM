17af8e78863aafc660e511ea7044ed7487c48bf9
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-02-21 14:10:47 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) 
+{
+
+	int n1,n2,sum;
+  
+	printf("\n enter the first number");
+  
+	scanf("%d",&n1);
+  
+	printf("\n enter the second number");
+	scanf("%d",&n2);
+	sum=n1+n2;
+	printf("%d",sum);
+	return 0;
+}
