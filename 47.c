30a58b58c87b5317c3c4880b76ca72599aab8a2e
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-02-21 13:58:05 +0530

    Create 47.c

---
+++ 47.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() 
+{
+	int n,i,a[10],min=99,max=0;
+	scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(i=0;i<n;i++)
+   {
+       if(a[i]>max)
+       {
+           max=a[i];
+       }
+   }
+   for(i=0;i<n;i++)
+   {
+       if(a[i]<min)
+       {
+           min=a[i];
+       }
+   }
+   printf("min:%d\nmax:%d",min,max);
+	return 0;
+}
