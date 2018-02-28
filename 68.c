2e31b897c905cab976020d7fc8191bcfabf13f6e
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-02-28 14:01:21 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+    int a;
+    scanf("%d",&a);
+    if(a%7==0)
+    printf("%d is the multiple of 7.",a);
+    else
+    printf("%d is not a multiple of 7.",a);
+	return 0;
+}
