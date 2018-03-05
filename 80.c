d64b6663f4759ee6cbdc7fc7a79c25f691d5bdf3
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-03-05 13:09:00 +0530

    Rename 79.c to 80.c

---
+++ 80.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int a,b,c,i;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	c=a*b;
+	for(i=2;i<c;i++)
+	{
+		if(c==i*i);
+		{
+			printf("yes.");
+			break;
+		}	
+	}
+	
+
+	
+
+}
