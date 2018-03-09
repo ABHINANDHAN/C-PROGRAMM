9908f22faf7b7df818b741c6fb0baa540c8399e6
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-03-09 16:00:45 +0530

    Create 90.c

---
+++ 90.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[30];
+    int i,n;
+    scanf("%s",a);
+    for(i=0;i<n;i++)
+     {
+         if(a[i]=='0'||a[i]=='1'||a[i]=='2'||a[i]=='3'||a[i]=='4'||a[i]=='5'||a[i]=='6'||a[i]=='7'||a[i]=='8'||a[i]=='9')
+         {
+             printf("%c",a[i].);
+         }
+     }
