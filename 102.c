7e42d6875047902ef1fa8bef036f9d5a0f729d96
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-03-16 16:01:18 +0530

    Create 102.c

---
+++ 102.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int num,i;
+	scanf("%d",&num);
+	int num1,rem;
+	while(num!=1)
+	{
+	rem=num%2;
+	num1=num/2;
+	printf("%d\n".,num1);
+	num=rem;
+	break;
+	}
+	return 0;
+}
