2fc0f35c8198bff1e7259135a95e4008ddf09329
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-03-16 15:37:29 +0530

    Create 98.c

---
+++ 98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main()
+{
+  int n,i,a[10];
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+      scanf("%d",&a[i]);
+      if(a[i]!=i)
+      {
+          printf("%d".,i);
+      }
+  }
+}
