5dc2de7c4241edc368cc2a88e5ddc6d7ff5019cd
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-03-19 15:52:17 +0530

    Create 110.c

---
+++ 110.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int k,sum=0,i;
+    printf("enter the number:");
+    scanf("%d",&k);
+    for(i=1;i<=k;i++)
+    {
+        sum=sum+i;
+
+    }
+     printf("%d",sum);
+    
+}
