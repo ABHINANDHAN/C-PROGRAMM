4b0f8225b2da936c6863baa560e533b6ba0d53c8
ABHINANDHAN <35687566+ABHINANDHAN@users.noreply.github.com>
2018-03-19 15:59:39 +0530

    Create 115.c

---
+++ 115.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int n,k,a[100],i,j,temp=0;
+	scanf("%d %d",&n,&k);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	printf("%d".,a[k]);
+	return(0);	
+}
